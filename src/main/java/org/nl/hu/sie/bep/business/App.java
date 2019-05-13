6deb84cd6bf467f2341ecf8da343dc846750e946
abosma <atillabosma@hotmail.com>
2019-05-13 14:48:39 +0200

    Maven Generated Initial Commit

---
+++ src/main/java/org/nl/hu/sie/bep/business/App.java
@@ -0,0 +1,13 @@
+package org.nl.hu.sie.bep.business;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
