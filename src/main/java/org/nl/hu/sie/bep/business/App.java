34748e49c7df7d99eb3d0f8608896b558ff9c73a
Dima <dmytro.shelomov@student.hu.nl>
2019-06-20 11:53:40 +0200

    nieuwe feature alles

--- src/main/java/org/nl/hu/sie/bep/business/App.java
+++ src/main/java/org/nl/hu/sie/bep/business/App.java
@@ -1,13 +1,28 @@
 package org.nl.hu.sie.bep.business;
 
+import org.nl.hu.sie.bep.business.models.Data;
+import org.nl.hu.sie.bep.business.models.KlantMysqlImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        try {
+            connect();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void connect() throws ClassNotFoundException, SQLException {
+        KlantMysqlImpl data = new KlantMysqlImpl();
+        List<Data> entries = data.get(5);
+
+        entries.forEach(System.out::println);
     }
 }
+
